d96ae72b39a88287425470c385033922e999a7e6
IT.SKC <it.skc@skc.kz>
2019-09-13 12:20:26 +0600

    cdn rules

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,4 @@ mix.js('resources/js/app.js', 'public/js')
 	.version()
     .sass('resources/sass/app.scss', 'public/css')
     .version()
-    .sass('resources/sass/static.scss', 'public/css')
     ;
