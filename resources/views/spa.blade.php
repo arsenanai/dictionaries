4053c276811e29b49e837f491b02cf7169611c31
IT.SKC <it.skc@skc.kz>
2019-09-13 12:09:37 +0600

    and again

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ENS TRU</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/static.css') }}">
 </head>
 <body>
     <div id="app">
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
 	.version()
     .sass('resources/sass/app.scss', 'public/css')
-    .version();
+    .version()
+    .sass('resources/sass/static.scss', 'public/css')
+    ;
